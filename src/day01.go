46330668368ca7229388473a8ffbc62f272c8b8b
zwrg <galekrafal1@gmail.com>
2024-12-01 12:09:24 +0100

    day 1

---
+++ src/day01.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"path/filepath"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+func part1(left []int, right []int) {
+	sort.Ints(left)
+	sort.Ints(right)
+
+	var diffSum = 0
+
+	for i := range left {
+		diffSum += int(math.Abs(float64(left[i] - right[i])))
+	}
+
+	println(diffSum)
+}
+
+func part2(left []int, right []int) {
+	countMap := make(map[int]int)
+	for _, number := range right {
+		countMap[number]++
+	}
+
+	sum := 0
+	for _, number := range left {
+		sum += number * countMap[number]
+	}
+
+	println(sum)
+}
+
+func main() {
+	filePath := filepath.Join("data", "2024", "day1.txt")
+
+	file, err := os.Open(filePath)
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
+	defer func(file *os.File) {
+		err := file.Close()
+		if err != nil {
+			fmt.Println("Error closing file:", err)
+		}
+	}(file)
+
+	scanner := bufio.NewScanner(file)
+	var leftColumn []int
+	var rightColumn []int
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		parts := strings.Fields(line)
+		num1, err1 := strconv.Atoi(parts[0])
+		num2, err2 := strconv.Atoi(parts[1])
+
+		if err1 != nil || err2 != nil {
+			fmt.Println("Error converting numbers:", err1, err2)
+			continue
+		}
+
+		leftColumn = append(leftColumn, num1)
+		rightColumn = append(rightColumn, num2)
+	}
+
+	part1(leftColumn, rightColumn)
+	part2(leftColumn, rightColumn)
+}
