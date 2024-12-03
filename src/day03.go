2cb142815d5d4e9cca91f3eadacb1a9ca0e44fc5
zwrg <galekrafal1@gmail.com>
2024-12-03 10:31:47 +0100

    day3

--- src/day03.go
+++ src/day03.go
@@ -5,14 +5,56 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"regexp"
+	"strconv"
 )
 
-func day3part1() int {
-	return 0
+func day3part1(content string) int {
+	re := regexp.MustCompile(`(?m)mul\((\d{1,3}),(\d{1,3})\)`)
+	matches := re.FindAllStringSubmatch(content, -1)
+	var sum int
+	for _, match := range matches {
+		x, _ := strconv.Atoi(match[1])
+		y, _ := strconv.Atoi(match[2])
+		sum += x * y
+	}
+	return sum
 }
 
-func day3part2() int {
-	return 0
+func day3part2(content string) int {
+	mulRegex := regexp.MustCompile(`mul\((\d{1,3}),(\d{1,3})\)`)
+	doRegex := regexp.MustCompile(`do\(\)`)
+	dontRegex := regexp.MustCompile(`don't\(\)`)
+
+	isEnabled := true
+	total := 0
+	i := 0
+
+	for i < len(content) {
+		switch {
+		case i+4 <= len(content) && doRegex.MatchString(content[i:i+4]):
+			isEnabled = true
+			i += 4
+		case i+7 <= len(content) && dontRegex.MatchString(content[i:i+7]):
+			isEnabled = false
+			i += 7
+		case i+12 <= len(content):
+			slice := content[i : i+12]
+			if matches := mulRegex.FindStringSubmatch(slice); matches != nil {
+				if isEnabled {
+					x, _ := strconv.Atoi(matches[1])
+					y, _ := strconv.Atoi(matches[2])
+					total += x * y
+				}
+				i += len(matches[0])
+			} else {
+				i++
+			}
+		default:
+			i++
+		}
+	}
+	return total
 }
 
 func main() {
@@ -30,12 +72,13 @@ func main() {
 		}
 	}(file)
 
+	var content string
 	scanner := bufio.NewScanner(file)
-
 	for scanner.Scan() {
 		line := scanner.Text()
+		content += line + "\n"
 	}
 
-	println(day3part1())
-	println(day3part2())
+	println(day3part1(content))
+	println(day3part2(content))
 }
